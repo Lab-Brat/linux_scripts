8dbb2d8ce5da4560bd371216a057b713d512e352
Lab-Brat <xoovoox@protonmail.com>
2022-03-24 23:45:42 +0300

    add port to ssh_keygen and remove : from config

--- ssh/ssh_keygen.sh
+++ ssh/ssh_keygen.sh
@@ -3,17 +3,19 @@ key_name=$1
 user=$2
 pass=$3
 host=$4
+port=$5
 user_ssh="$HOME/.ssh"
 
 ssh-keygen -f "$user_ssh/$key_name" -P "" -q
 #cat "$user_ssh/$key_name.pub"
 
 sshpass -p $pass ssh-copy-id -o StrictHostKeyChecking=no \
-	-i $user_ssh/$key_name $user@$host
+	-i $user_ssh/$key_name -p $port $user@$host
 
 cat << EOF >> "$user_ssh/config"
-Host $host:
+Host $host
 	User $user
+	Port $port
 	IdentityFile $user_ssh/$key_name
 
 EOF
