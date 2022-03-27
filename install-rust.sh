bf5f2131d28cc91902df60470dce509a1d0e0312
Lab-Brat <xoovoox@protonmail.com>
2022-03-27 15:35:00 +0300

    add os check and development tools install

--- install-rust.sh
+++ install-rust.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# run ansible playbook to install build-essential
+# detect os and install build-essential
+osfam=$(cat /etc/os-release | grep -i id_like | tr -d '"' | sed 's/.*=//')
+if [ "$osfam" == 'debian' ]; then
+	sudo apt install build-essential
+else
+	sudo dnf group install "Development Tools"
+fi
 
 # download and run installer
 curl https://sh.rustup.rs -sSf | sh
