159aa092766ea408e4f63e1e2bb434057e340ee5
Lab-Brat <xoovoox@protonmail.com>
2022-04-24 22:56:50 +0300

    download depends to ./packs, user input on the same line

--- get_packs/get_depends.sh
+++ get_packs/get_depends.sh
@@ -2,7 +2,8 @@
 # script reads the depencies list from depends.txt line by line,
 # downloads if available, if error is encountered, record in err.txt
 #
-echo "input package name: "
+echo "" > dwn.txt
+echo -n "input package name: "
 read pack
 
 apt-cache showpkg $pack | awk '/Dependencies:/,/Provides:/ {for(i=1;i<NF;i++) {if(i%3==0) {print $i}}}' | tee depends.txt
@@ -14,5 +15,6 @@ for LINE in $LINES
 do
 	apt-get download --print-uris "$LINE" | grep ^\' | cut -d\' -f2 \
 	1>> dwn.txt 2>> err.txt
+	wget -nv -P ./packs $(tail -n 1 dwn.txt) 
 done
 
