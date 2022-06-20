bcbc5470878ee719c67a57f0fb74eb55ba7677c4
Lab-Brat <xoovoox@protonmail.com>
2022-06-20 23:05:52 +0300

    my entry for "how-to-exit-vim"

---
+++ exitVim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+vim_num=$(ps aux | grep vim | wc -l)
+
+if [ $vim_num -gt 1 ]
+then
+	pids=$(ps aux | grep vim | head -n $(($vim_num-1)) |  awk '{print $2}')
+	for value in $pids
+	do
+	    echo "[# Terminating Process $value #]"
+	    sudo kill -9 $value
+	done
+
+else
+	echo "[# Vim is NOT Running #]"
+fi
+
