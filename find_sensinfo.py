8f32308ad301666e50e1138b7301d078eff38269
LabBrat <xoovoox@protonmail.com>
2022-08-07 14:56:50 +0300

    fix major bugs

--- find_sensinfo.py
+++ find_sensinfo.py
@@ -4,9 +4,18 @@ import subprocess
 
 path = str(sys.argv[1])
 try:
-    exclude_dir = str(sys.argv[2])
+    exclude_dir = sys.argv[2]
 except:
-    exclude_dir = '{venv,__pycache__}'
+    exclude_dir = 'venv,__pycache__'
+
+def split_dir(exclude_dir):
+    split_dir = exclude_dir.split(',')
+    flags = ('--exclude-dir,'*len(split_dir)).split(',')
+    all = []
+    for dir, flag in zip(split_dir, flags):
+        all.append(flag)
+        all.append(dir)
+    return all
 
 def get_output(command):
     return subprocess.run(command, capture_output=True)
@@ -15,9 +24,12 @@ def print_decode_output(output):
     for line in output.stdout.decode('utf-8').split('\n'):
         print(line)
 
-ip_regex = '"\\b([0-9]{1,3}\.){3}[0-9]{1,3}\\b"'
-grep_ip = ["grep", "-roE", ip_regex, exclude_dir, path]
-grep_pass = ["grep", "-ri", "password", exclude_dir, path]
+ip_regex = "\\b([0-9]{1,3}\.){3}[0-9]{1,3}\\b"
+grep_ip = ["grep", "-roE", ip_regex, path]
+grep_ip.extend(split_dir(exclude_dir))
+grep_pass = ["grep", "-ri", "password", path]
+grep_pass.extend(split_dir(exclude_dir))
+
 print("Found IP addresses: ")
 print_decode_output(get_output(grep_ip))
 print("Found Passwords: ")
