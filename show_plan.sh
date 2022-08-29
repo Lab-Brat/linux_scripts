20335a2b30280e41fa08cc588827faeec910f4a8
LabBrat <xoovoox@protonmail.com>
2022-08-29 22:33:31 +0300

    add display all planned months

--- show_plan.sh
+++ show_plan.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-plan='/path/to/plan/__PLAN__.md'
+plan='/home/labbrat/Documents/notes/__PLAN__.md'
+
+
+if [[ $1 == 'all' ]]
+then
+	for i in {0..11}
+	do
+		date=$(date --date="$(date +%Y-%m-15) +$i month" +%b)
+		if [[ $(grep $date $plan) != '' ]]
+		then
+			echo "===== Plans for $date ====="
+			grep $date $plan | awk '{$(NF--)=""; print}'
+			echo ""
+		fi
+	done
+	exit 0
+fi
+
 
 if [[ $1 == 'next' ]]
 then
