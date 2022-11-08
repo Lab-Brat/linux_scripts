dee56d7726e9aae06b55abe5c6c69586900723f0
LabBrat <xoovoox@protonmail.com>
2022-11-08 23:21:53 +0300

    use whois instead

--- find_ip_info.py
+++ find_ip_info.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+import subprocess
 import requests
-import json
 import sys
 
 def get_info(ip):
@@ -10,21 +10,44 @@ def get_info(ip):
     url = f"https://iplist.cc/api/{ip}"
     return requests.get(url).json()
 
+def _get_grep(property, ip):
+    '''
+    grep for a particular property
+    '''
+    result = subprocess.check_output(
+                 f'whois {ip} | grep -i {property}', 
+                 shell = True, text = True)
+    return result.split(':')[-1].replace(' ', '').replace('\n', '')
+
+def get_info_local(ip):
+    '''
+    Use locally installed whois command to get information
+    '''
+    orgname = _get_grep('orgname', ip)
+    country = _get_grep('country', ip)
+    return {'ip': ip, 'org': orgname, 'country': country}
+
 def pretty_print(response):
     '''
     Print only relevant information
     '''
     print(f"IP:       {response['ip']}")
-    print(f"Provider: {response['asn']['name']}")
-    print(f"Country:  {response['countryname']}\n")
+    print(f"Provider: {response['org']}")
+    print(f"Country:  {response['country']}\n")
 
 
 # Parse all IP addresses from CLI and search info
 try:
     for ip in sys.argv[1].split(','):
-        pretty_print(get_info(ip))
+        # ip_info = get_info(ip)
+        # orgname = ip_info['asn']['name']
+        # country = ip_info['countryname']
+        ip_info  = get_info_local(ip)
+        pretty_print(ip_info)
 except IndexError:
     print('Argument format: ip or ip1,ip2,ip3')
+
+# get_info_local('159.223.209.128')
 
 # Run in terminal
 # ./find_ip_info.py 8.8.8.8
