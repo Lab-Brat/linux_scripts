989fdd3886462a9aab5086d0d787bb0b08d7f2fb
LabBrat <xoovoox@protonmail.com>
2022-11-18 23:40:50 +0300

    add some formatting

--- admin.py
+++ admin.py
@@ -4,8 +4,12 @@ import os
 
 @click.command()
 def show():
-    files = [f for f in os.listdir('.') if f[-2::] in ['py', 'sh']]
-    click.echo(files)
+    files = [f for f in os.listdir('.') if f[-3::] in ['.py', '.sh']]
+    files.remove('admin.py')
+    click.echo('[Available Scripts]')
+    for i, file in enumerate(files):
+        click.echo(f'({i+1}) {file}')
+    click.echo('\n')
 
 if __name__ == '__main__':
     show()
