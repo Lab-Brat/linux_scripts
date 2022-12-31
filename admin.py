4037aff868e1a9ffbdc626ca0114878cb2e09366
Lab-Brat <xoovoox@protonmail.com>
2022-12-31 17:31:04 +0300

    add ssh_connect to admin cli

--- admin.py
+++ admin.py
@@ -6,7 +6,7 @@ import ssh_connect
 
 @click.group(invoke_without_command=False)
 @click.pass_context
-def adm(ctx):
+def adm(empty):
     pass
 
 @adm.command()
@@ -16,6 +16,19 @@ def show():
     click.echo('[Available Scripts]')
     for i, file in enumerate(files):
         click.echo(f'({i+1}) {file}')
+
+@adm.command()
+@click.option('-n', '--name', required=True, type=str)
+@click.option('-h', '--host', required=True, type=str)
+@click.option('-t', '--terminal', is_flag=True)
+@click.option('-c', '--command', type=str)
+def ssh(name, host, terminal, command):
+    sc = ssh_connect.SSHConnect(name, host)
+    if terminal:
+        sc.terminal()
+    elif command:
+        sc.cmd(command)
+
 
 if __name__ == '__main__':
     adm()
