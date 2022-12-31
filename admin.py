36abe4651cc2cc356582317947bd1c223b3d348e
Lab-Brat <xoovoox@protonmail.com>
2022-12-31 17:03:18 +0300

    use nested commands

--- admin.py
+++ admin.py
@@ -1,15 +1,21 @@
 #!venv/bin/python
 import click
 import os
+import ssh_connect
 
-@click.command()
+
+@click.group(invoke_without_command=False)
+@click.pass_context
+def adm(ctx):
+    pass
+
+@adm.command()
 def show():
     files = [f for f in os.listdir('.') if f[-3::] in ['.py', '.sh']]
     files.remove('admin.py')
     click.echo('[Available Scripts]')
     for i, file in enumerate(files):
         click.echo(f'({i+1}) {file}')
-    click.echo('\n')
 
 if __name__ == '__main__':
-    show()
+    adm()
