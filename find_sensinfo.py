f162798594e7599303fca61f2931902878e91ae7
Lab-Brat <xoovoox@protonmail.com>
2023-01-05 22:23:20 +0300

    make it to be more modular

--- find_sensinfo.py
+++ find_sensinfo.py
@@ -2,19 +2,8 @@
 import sys
 import subprocess
 
-try:
-    path = str(sys.argv[1])
-except IndexError:
-    print('[ERROR!] Please specify grep location')
-    sys.exit(1)
 
-try:
-    exclude_dir = sys.argv[2]
-except IndexError:
-    print('[WARNING!] exclude-dir not specified, '
-          'using defauls: venv,__pycache__')
-    exclude_dir = 'venv,__pycache__'
-
+# ------------------------------ Script Logic ------------------------------- #
 def split_dir(exclude_dir):
     '''
     Read a string of excluded directories,
@@ -37,16 +26,38 @@ def print_decode_output(output):
     for line in output.stdout.decode('utf-8').split('\n'):
         print(line)
 
-ip_regex = "\\b([0-9]{1,3}\.){3}[0-9]{1,3}\\b"
-grep_ip = ["grep", "-roE", ip_regex, path]
-grep_ip.extend(split_dir(exclude_dir))
-grep_pass = ["grep", "-ri", "password", path]
-grep_pass.extend(split_dir(exclude_dir))
+def find_info(path, exclude_dir=''):
+    '''
+    Grep regular expressions, output all matches.
+    '''
+    ip_regex = "\\b([0-9]{1,3}\.){3}[0-9]{1,3}\\b"
+    grep_ip = ["grep", "-roE", ip_regex, path]
+    grep_ip.extend(split_dir(exclude_dir))
+    grep_pass = ["grep", "-ri", "password", path]
+    grep_pass.extend(split_dir(exclude_dir))
+    
+    print("Found IP addresses: ")
+    print_decode_output(get_output(grep_ip))
+    print("Found Passwords: ")
+    print_decode_output(get_output(grep_pass))
 
-print("Found IP addresses: ")
-print_decode_output(get_output(grep_ip))
-print("Found Passwords: ")
-print_decode_output(get_output(grep_pass))
+
+# ------------------------------- Run Script -------------------------------- #
+if __name__ == '__main__':
+    try:
+        path = str(sys.argv[1])
+    except IndexError:
+        print('[ERROR!] Please specify grep location')
+        sys.exit(1)
+
+    try:
+        exclude_dir = sys.argv[2]
+    except IndexError:
+        print('[WARNING!] exclude-dir not specified, '
+            'using defauls: venv,__pycache__')
+        exclude_dir = 'venv,__pycache__'
+
+    find_info(path, exclude_dir)
 
 # run in terminal
 # ./find_sensinfo.py /opt/data 'folder1,folder2,venv'
