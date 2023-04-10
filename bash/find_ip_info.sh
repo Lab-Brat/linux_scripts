8e4d17846d4959fdd7b8e17b5623d400f43ea3fa
Lab-Brat <xoovoox@protonmail.com>
2023-04-10 22:52:47 +0300

    shorten the syntax

--- bash/find_ip_info.sh
+++ bash/find_ip_info.sh
@@ -9,24 +9,12 @@ domain=$1
 whois_output=$(whois "$domain")
 
 # Extract relevant information using regular expressions
-if [[ -n $(echo $whois_output | grep -i amazon ) ]]
-then
-	owner="AWS"
-elif [[ -n $(echo $whois_output | grep -i azure ) ]]
-then
-	owner="Azure"
-elif [[ -n $(echo $whois_output | grep -i hetzner ) ]]
-then
-	owner="Hetzner"
-elif [[ -n $(echo $whois_output | grep -i hivelocity ) ]] 
-then
-	owner="Hivelocity"
-elif [[ -n $(echo $whois_output | grep -i digitalocean ) ]]
-then
-	owner="DigitalOcean"
-elif [[ -n $(echo $whois_output | grep -i atman ) ]]
-then
-	owner="Atman"
+if [[ -n $(echo $whois_output |   grep -i amazon ) ]] ; then  owner="AWS"
+elif [[ -n $(echo $whois_output | grep -i atman ) ]] ;  then  owner="Atman"
+elif [[ -n $(echo $whois_output | grep -i azure ) ]] ;  then  owner="Azure"
+elif [[ -n $(echo $whois_output | grep -i hetzner ) ]] ;    then  owner="Hetzner"
+elif [[ -n $(echo $whois_output | grep -i hivelocity ) ]] ; then  owner="Hivelocity"
+elif [[ -n $(echo $whois_output | grep -i digitalocean ) ]] ; then  owner="DigitalOcean"
 else
 	owner=$(echo "$whois_output" | grep -E -i "org-name|orgname" \
 	                             | tail -n 1 \
