0b91d8e3fa8d5a12cca04438088c4e0d4ef09bd7
Lab-Brat <xoovoox@protonmail.com>
2023-05-31 17:14:03 +0300

    fix some variables in get_fw_sources.sh

--- bash/get_fw_sources.sh
+++ bash/get_fw_sources.sh
@@ -4,14 +4,14 @@ zone="${1:-public}"
 sources=$(sudo firewall-cmd --zone "${zone}" --list-sources | tr " " "\n" | sort -h)
 
 echo "Networks:"
-for source in "${sources}"; do
+for source in $sources; do
     if echo "${source}" | grep -q '/' && ! echo "${source}" | grep -q '/32'; then
         echo "- $source"
     fi
 done
 
 echo -e "\nIP addresses:"
-for source in "${sources}"; do
+for source in $sources; do
     if echo "${source}" | grep -q '/32' || ! echo "${source}" | grep -q '/'; then
         echo "- \"${source%/*}\""
     fi
