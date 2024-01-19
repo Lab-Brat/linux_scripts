42f9fcd15268ec4b7d6f6ef24cf3efa17e575cc4
Lab-Brat <xoovoox@protonmail.com>
2024-01-19 23:32:54 +0300

    add categorize_addresses method

--- fw_iplist_checker.py
+++ fw_iplist_checker.py
@@ -1,9 +1,12 @@
 import re
+import ipaddress
 
 
 class IPChecker:
     def __init__(self, filepath) -> None:
-        self.iplist = self.read_file(filepath)
+        iplist = self.read_file(filepath)
+        ips, networks, invalid = self.categorize_addresses(iplist)
+        print(len(ips), len(networks), len(invalid))
 
         print("\nAll checks completed :)")
 
@@ -12,6 +15,27 @@ class IPChecker:
             ip_pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?:/\d{1,2}|)"
             one_string = "".join(file.readlines())
             return re.findall(ip_pattern, one_string)
+
+    def categorize_addresses(self, iplist):
+        ips = []
+        networks = []
+        invalid = []
+        for address in iplist:
+            try:
+                if "/" in address:
+                    networks.append(ipaddress.IPv4Network(address))
+                else:
+                    ips.append(ipaddress.IPv4Address(address))
+            except:
+                invalid.append(address)
+
+        if invalid:
+            print(f"invalid addresses found:")
+            for address in invalid:
+                print(address)
+
+        return ips, networks, invalid
+
 
 if __name__ == "__main__":
     filepath = "./inventory"
