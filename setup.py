ed5cedd626bfa368fc17f81c119ae14b3513adfe
Lab-Brat <xoovoox@protonmail.com>
2024-01-20 23:24:31 +0300

    configure setup tools to package everything into ladm executable

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="admin",
-    version="0.1.0",
-    py_modules=["yourscript"],
-    install_requires=[
-        "Click",
-        "paramiko",
-        "requests",
-        "jinja2",
-    ],
-    entry_points={
-        "console_scripts": [
-            "admin = admin:adm",
-        ],
-    },
-)
+setup()
