48faea802186d0dfc9f9c93872c74f4c02b867fa
Lab-Brat <xoovoox@protonmail.com>
2024-01-24 00:03:11 +0300

    fix shell scrip paths in manifest file

--- src/ladm/admin_cli.py
+++ src/ladm/admin_cli.py
@@ -5,7 +5,7 @@ import json
 from ladm import ssh_connect, systemd
 
 full_path = os.path.dirname(__file__)
-shell_dir = f"{full_path}/src/ladm/bash"
+shell_dir = f"{full_path}/bash"
 
 @click.group(invoke_without_command=False)
 @click.pass_context
