df0b7cb618c9ff2e82645555df2b09f74853255f
Lab-Brat <xoovoox@protonmail.com>
2024-01-31 22:10:50 +0300

    write general settings into ssh.config

--- src/ladm/ssh_config.py
+++ src/ladm/ssh_config.py
@@ -1,15 +1,20 @@
 import os
 import yaml
+from pprint import pprint
+
+home_dir = os.getenv("HOME")
 
 
 class SSH_Config:
     def __init__(self, yaml_config=None) -> None:
-        home_dir = os.getenv("HOME")
         config_dir = ".ladm"
         default_conf = f"{home_dir}/{config_dir}/ssh_conf.yaml"
 
         yaml_config = yaml_config if yaml_config else default_conf
         self.config = self.read(yaml_config)
+        # pprint(self.config)
+        self.indent = "  "
+        self.ssh_config = []
 
     def read(self, yaml_config):
         with open(yaml_config, "r") as file:
@@ -19,7 +24,25 @@ class SSH_Config:
         with open(output_yaml, "w+") as file:
             yaml.dump(self.config, file)
 
+    def _create_general_settings(self, settings):
+        self.ssh_config.append("Host *")
+        for setting in settings:
+            self.ssh_config.append(f"{self.indent}{setting}")
+
+    def _create_host_settings(self):
+        pass
+
+    def _write_ssh_config(self, location=home_dir):
+        with open(f"{location}/config", "w+") as file:
+            for line in self.ssh_config:
+                file.write(f"{line}\n")
+
+    def create_config(self):
+        general_settings = self.config["general_settings"]
+        self._create_general_settings(general_settings)
+        self._write_ssh_config("./")
+
 
 if __name__ == "__main__":
     output_conf = "./new_ssh_conf.yaml"
-    SSH_Config().write(output_conf)
+    SSH_Config().create_config()
